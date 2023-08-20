8bd7c511140fceec2d04fcdf18427e4b87d63ccf
Luis Jackniel Arellano <rayreckl.j.a.s01@gmail.com>
2023-08-20 16:03:55 -0700

    Android deploy

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.miganado.app',
   appName: 'Mi Ganado',
-  webDir: 'dist',
+  webDir: 'dist/mi-ganado',
   server: {
     androidScheme: 'https'
   }
